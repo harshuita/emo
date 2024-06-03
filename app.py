7eaba1e5c1ed7de9e5f2646f9ec54caf6b254ad8
harshuita <harshitab1004@gmail.com>
2024-06-03 15:07:31 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+from transformers import pipeline
+app = Flask(__name__)
+sentiment_analysis = pipeline('sentiment-analysis')
+@app.route('/')
+def home():
+   return render_template('index.html')
+@app.route('/result', methods=['POST','GET'])
+def result():
+    if request.method == 'POST':
+        text = request.form['text']
+        results = sentiment_analysis(text)
+        sentiment = results[0]['label']
+   return render_template('result.html')
+if __name__ == '__main__':
+   app.run()
